71321c62a7708e96ee83b07b9b5e133ec75b1323
Edgar Solis Vizcarra <edgar21dejunio@gmail.com>
2017-04-21 21:13:28 -0500

    WORKING VERSION 1.0 This version is fully functional but it doesn't have the code to control screens, it must be added later

--- bolis2.ino
+++ bolis2.ino
@@ -18,63 +18,88 @@
 
 
 ////Pin connected to IN1 of SSR
-int relay1 = 5;
+int relay1 = 6;
 ////Pin connected to IN2 of SSR
-int relay2 = 6;
-////Pin connected to button
-int buttonPin = 2;     // the number of the pushbutton pin
+int relay2 = 7;
+////Pin connected to buttons
+int button1Pin = 2;     // the number of the pushbutton pin
+int button2Pin = 3;     // the number of the pushbutton pin
+int button3Pin = 4;     // the number of the pushbutton pin
+int button4Pin = 5;     // the number of the pushbutton pin
 ////Pin connected to sensor
-int sensorPin = 3;     // the number of the sensor pin
-////Pin connected to sensor
-int currentPin = 7;
+int sensorPin = A0;     // the number of the sensor pin
 
 ////VARIABLES
 int sp1 = 0;
 int sp2 = 0;
 int sensorState = LOW;
 unsigned long previousMillis = 0;
+unsigned long previousMillis2 = 0;
 
 ////CONSTANTS
-int val1 = 300;
-int val2 = 300;
-long onTime = 300;
-int buttonState = 0;         // variable for reading the pushbutton status
+int baseTime = 100;
+int val1 = baseTime;
+int val2 = baseTime;
+long onTime = baseTime;
+int button1State = 0;         // variable for reading the pushbutton status
+int button2State = 0;         // variable for reading the pushbutton status
+int button3State = 0;         // variable for reading the pushbutton status
+int button4State = 0;         // variable for reading the pushbutton status
 
 
 
 void setup() {
   //set pins to output so you can control the shift register
-//  pinMode(latchPin, OUTPUT);
-//  pinMode(clockPin, OUTPUT);
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
-//  pinMode(dataPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(button1Pin, INPUT);
+  pinMode(button1Pin, INPUT);
+  pinMode(button1Pin, INPUT);
+  pinMode(button1Pin, INPUT);
   pinMode(sensorPin, INPUT);
-  pinMode(currentPin, INPUT);
-  digitalWrite(currentPin, HIGH);
 }
 
 ////FUNCTIONS
   TimedAction buttonThread = TimedAction(100,buttonStatus);
 
 void buttonStatus (){
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+  button3State = digitalRead(button3Pin);
+  button4State = digitalRead(button4Pin);
+  if (button1State == LOW) {
     sp1 ++;
+  }
+  if (button2State == LOW) {
+    sp1 --;
+  }
+  if (button3State == LOW) {
+    sp2 ++;
+  }
+  if (button4State == LOW) {
+    sp2 --;
   }
 }
 
 void loop() {
-  sensorState = digitalRead(sensorPin);
+  sensorState = analogRead(sensorPin);
   unsigned long currentMillis = millis();
-  if (sensorState == HIGH){
+  unsigned long currentMillis2 = millis();
+  if (sensorState > 100){
     previousMillis = currentMillis;
     digitalWrite(relay1, HIGH);
   }
-  if ((sensorState == LOW) && (currentMillis - previousMillis >= ((onTime-sp1) * 10))){
+  if ((sensorState < 100) && (currentMillis - previousMillis >= ((onTime-sp1) * 10))){
     previousMillis = currentMillis;
-    digitalWrite(relay1, LOW);
+      digitalWrite(relay1, LOW);
+  }
+  if (sensorState > 100){
+    previousMillis2 = currentMillis2;
+    digitalWrite(relay2, HIGH);
+  }
+  if ((sensorState < 100) && (currentMillis2 - previousMillis2 >= ((onTime-sp2) * 10))){
+    previousMillis2 = currentMillis2;
+      digitalWrite(relay2, LOW);
   }
   buttonThread.check();
 }
